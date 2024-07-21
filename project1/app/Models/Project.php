8abdb1ee51603c8279f112156b8479c3bd85bd01
Ignacio Donoso <ignacio.donoso.s@gmail.com>
2024-07-21 02:25:47 -0400

    FIX: dates in static methods for get and getAll

--- project1/app/Models/Project.php
+++ project1/app/Models/Project.php
@@ -132,8 +132,8 @@ class Project
 
     public static function getAll()
     {
-        $project = new Project(1, 'Proyecto 1', Date::now()->toDateTimeString(), 'Activo', 'Juan Perez', 1000);
-        $project2 = new Project(2, "Proyecto 2", Date::now()->toDateTimeString(), 'Activo', 'Juan Perezzz', 1000);
+        $project = new Project(1, 'Proyecto 1', "14/12/1986", 'Activo', 'Juan Perez', 1000);
+        $project2 = new Project(2, "Proyecto 2", "14/12/1986", 'Activo', 'Juan Perezzz', 1000);
         $projects[0] = $project;
         $projects[1] = $project2;
         return $projects;
@@ -141,7 +141,7 @@ class Project
 
     public static function getById($id)
     {
-        $project = new Project($id, "Proyecto $id", Date::now()->toDateTimeString(), 'Activo', 'Juan Perez', 1000);
+        $project = new Project($id, "Proyecto $id", "14/12/1986", 'Activo', 'Juan Perez', 1000);
         return $project;
     }
 
